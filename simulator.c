c7df1dfbf8d1f0de5a37ac0baa69872ede963ae9
nistha07 <nisthadhakal07@gmail.com>
2025-12-22 22:22:15 +0545

    Tested queue operations in simulator

---
+++ simulator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "queue.h"
+
+int main() {
+    Queue testQueue;
+
+    initQueue(&testQueue);
+
+    printf("Adding vehicles to queue...\n");
+    enqueue(&testQueue, 101);
+    enqueue(&testQueue, 102);
+    enqueue(&testQueue, 103);
+
+    printf("Removing vehicles from queue...\n");
+    while (!isEmpty(&testQueue)) {
+        int vehicle = dequeue(&testQueue);
+        printf("Vehicle %d passed the junction\n", vehicle);
+    }
+
+    return 0;
+}
+
