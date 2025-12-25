74d4ab0e8d4c47a6986a44f31385b36be0eb4ac1
nistha07 <nisthadhakal07@gmail.com>
2025-12-25 20:43:37 +0545

    Created basic traffic generator program

--- traffic_generator.c
+++ traffic_generator.c
@@ -1,1 +1,31 @@
--
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    FILE *fa, *fb, *fc, *fd;
+
+    srand(time(NULL));
+
+    fa = fopen("laneA.txt", "a");
+    fb = fopen("laneB.txt", "a");
+    fc = fopen("laneC.txt", "a");
+    fd = fopen("laneD.txt", "a");
+
+    if (!fa || !fb || !fc || !fd) {
+        printf("Error opening lane files\n");
+        return 1;
+    }
+
+    fprintf(fa, "%d\n", rand() % 1000);
+    fprintf(fb, "%d\n", rand() % 1000);
+    fprintf(fc, "%d\n", rand() % 1000);
+    fprintf(fd, "%d\n", rand() % 1000);
+
+    fclose(fa);
+    fclose(fb);
+    fclose(fc);
+    fclose(fd);
+
+    return 0;
+}
